20f6a0092976653da8ce07da09ab751cee463452
Victoria Cheng <victoria.cheng15@gmail.com>
2025-05-19 16:50:21 -0400

    update wc tool (#6)
    * fix: utilize module keyword
    
    * feat: include parse_option tests
    
    * format files

--- test/wc_test.rb
+++ test/wc_test.rb
@@ -4,10 +4,50 @@ require 'minitest/autorun'
 require 'stringio'
 require_relative '../lib/wc'
 
+describe 'WC command - parse_option -l' do
+  it 'parse -l option' do
+    options, = WcTool.parse_options(['-l', 'test.txt'])
+    _(options[:lines]).must_equal true
+    _(options[:words]).must_equal false
+    _(options[:chars]).must_equal false
+    _(options[:bytes]).must_equal false
+  end
+end
+
+describe 'WC command - parse_option -w' do
+  it 'parse -w option' do
+    options, = WcTool.parse_options(['-w', 'test.txt'])
+    _(options[:lines]).must_equal false
+    _(options[:words]).must_equal true
+    _(options[:chars]).must_equal false
+    _(options[:bytes]).must_equal false
+  end
+end
+
+describe 'WC command - parse_option -m' do
+  it 'parse -m option' do
+    options, = WcTool.parse_options(['-m', 'test.txt'])
+    _(options[:lines]).must_equal false
+    _(options[:words]).must_equal false
+    _(options[:chars]).must_equal true
+    _(options[:bytes]).must_equal false
+  end
+end
+
+describe 'WC command - parse_option -c' do
+  it 'parse -c option' do
+    options, = WcTool.parse_options(['-c', 'test.txt'])
+    _(options[:lines]).must_equal false
+    _(options[:words]).must_equal false
+    _(options[:chars]).must_equal false
+    _(options[:bytes]).must_equal true
+  end
+end
+
 describe 'WC command - process_input' do
   it 'returns zero counts for empty input' do
     input = StringIO.new('')
-    counts = process_input(input, 'empty_input')
+    counts = WcTool.process_input(input, 'empty_input')
 
     # _ is a shorthand for expect
     # could write like expect(counts[:lines]).must_equal 0
@@ -20,7 +60,7 @@ describe 'WC command - process_input' do
 
   it 'counts lines, words, and bytes correctly for a single line' do
     input = StringIO.new("hello world\n")
-    counts = process_input(input, 'single_line')
+    counts = WcTool.process_input(input, 'single_line')
 
     _(counts[:lines]).must_equal 1
     _(counts[:words]).must_equal 2
@@ -33,7 +73,7 @@ end
 describe 'WC command - process_input with stdin' do
   it 'handles input from stdin (simulated with StringIO)' do
     input = StringIO.new("one two three\nfour five\n")
-    counts = process_input(input, '-')
+    counts = WcTool.process_input(input, '-')
 
     _(counts[:lines]).must_equal 2
     _(counts[:words]).must_equal 5

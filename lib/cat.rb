eb0155f7b83ac2610a7766f532ab8cd858c30a52
Victoria Cheng <victoria.cheng15@gmail.com>
2025-05-22 07:13:30 -0400

    add cat tool (#5)
    * add cat related files in bin, lib and test folders
    
    * chore: move all sample files to samples folder
    
    * update wc tool (#6)
    
    * fix: utilize module keyword
    
    * feat: include parse_option tests
    
    * format files
    
    * ci: fix RuboCop offenses
    
    * fix: add self to default_output method (#7)
    
    * test: add test cases for print outputs
    
    * feat: include the logics to print lines based on -n or -b
    
    * chore: include run_cli
    
    * ci: fix RuboCop offenses
    
    * chore: move cli method to bin/wc (#8)
    
    * add the ability to read from stdin
    
    * ci: fix RuboCop offenses
    
    * add ability to read 2 files for unit test and the logic
    
    * clean up lines in run_cli
    
    * ci: fix RuboCop offenses
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ lib/cat.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+module CatTool
+  def self.option_definition(options)
+    OptionParser.new do |opts|
+      opts.banner = 'Usage: cat_tool.rb [options] filename'
+      opts.on('-n', '--number', 'Number all output lines') do
+        options[:number_lines] = true
+      end
+      opts.on('-b', '--number-nonblank', 'Number non-blank output lines') do
+        options[:number_nonblank_lines] = true
+      end
+      opts.on('-h', '--help', 'Display help message') do
+        puts opts
+        exit
+      end
+    end
+  end
+
+  def self.parse_options(args)
+    options = {
+      number_lines: false,
+      number_nonblank_lines: false
+    }
+    parser = option_definition(options)
+    parser.parse!(args)
+    [options, args]
+  end
+
+  def self.print_output(content, options)
+    if options[:number_lines]
+      content.each_line.with_index(1) do |line, index|
+        print "\t#{index} #{line}"
+      end
+    elsif options[:number_nonblank_lines]
+      line_number = 1
+      content.each_line do |line|
+        if line.strip.empty?
+          print line
+        else
+          print "\t#{line_number} #{line}"
+          line_number += 1
+        end
+      end
+    else
+      print content
+    end
+  end
+
+  def self.process_input(content, options)
+    text = content.respond_to?(:read) ? content.read : content
+    text.force_encoding('BINARY')
+
+    print_output(text, options)
+  end
+
+  def self.check_file_existence(filename)
+    return if File.exist?(filename)
+
+    notify_error("File not found: #{filename}")
+  end
+
+  def self.run_cli(argv)
+    options, args = parse_options(argv)
+
+    if args.empty?
+      content = $stdin.read
+      process_input(content, options)
+    else
+      args.each do |filename|
+        if File.exist?(filename)
+          File.open(filename, 'r') do |file|
+            content = file.read
+            process_input(content, options)
+          end
+        else
+          puts "File not found: #{filename}"
+        end
+      end
+    end
+  end
+end

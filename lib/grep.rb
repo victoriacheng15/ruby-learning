e46b53f9565727d5d4ca7c7d140ae41733e98985
Victoria Cheng <victoria.cheng15@gmail.com>
2025-05-24 20:00:08 -0400

    add parse options for test cases and abilit to read args

--- lib/grep.rb
+++ lib/grep.rb
@@ -3,7 +3,43 @@
 require 'optparse'
 
 module GrepTool
+  def self.option_definition(options)
+    OptionParser.new do |opts|
+      opts.banner = 'Usage: grep_tool.rb [options] pattern filename'
+      opts.on('-i', '--ignore-case', 'Ignore case distinctions') do
+        options[:ignore_case] = true
+      end
+      opts.on('-v', '--invert-match', 'Invert the sense of matching') do
+        options[:invert_match] = true
+      end
+    end
+  end
+
+  def self.parse_options(args)
+    options = {
+      ignore_case: false,
+      invert_match: false
+    }
+    parser = option_definition(options)
+    parser.parse!(args)
+    [options, args]
+  end
+
   def self.run_cli(_argv)
-    puts 'GrepTool is not implemented yet.'
+    options, args = parse_options(_argv)
+
+    if args.size< 2
+      puts "Error: Missing pattern or filename."
+      puts "Usage: grep_tool.rb [options] pattern filename"
+      exit 1
+    end
+
+    pattern = args[0]
+    filename = args[1]
+
+
+    puts "Options: #{options.inspect}"
+    puts "Pattern: #{pattern.inspect}"
+    puts "Filename: #{filename.inspect}"
   end
 end
--- test/grep_test.rb
+++ test/grep_test.rb
@@ -2,4 +2,33 @@
 
 require 'minitest/autorun'
 require 'stringio'
-require_relative '../lib/cat'
+require_relative '../lib/grep'
+
+describe "Grep command - parse options" do
+  before do
+    @options = {
+      ignore_case: false,
+      invert_match: false
+    }
+  end
+
+  it "should parse -i option" do
+    args = ['-i', 'pattern', 'test.txt']
+    options, _args = GrepTool.parse_options(args)
+
+    _(options[:ignore_case]).must_equal true
+    _(options[:invert_match]).must_equal false
+    _(args[0]).must_equal 'pattern'
+    _(args[1]).must_equal 'test.txt'
+  end
+
+  it "should parse -v option" do
+    args = ['-v', 'pattern', 'test.txt']
+    options, _args = GrepTool.parse_options(args)
+
+    _(options[:ignore_case]).must_equal false
+    _(options[:invert_match]).must_equal true
+    _(args[0]).must_equal 'pattern'
+    _(args[1]).must_equal 'test.txt'
+  end
+end

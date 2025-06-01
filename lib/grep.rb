d642a8772267c1a7fd2786b3e04d5b3523c4caaa
Victoria Cheng <victoria.cheng15@gmail.com>
2025-06-01 18:32:51 -0400

    grep tool (#10)
    * initial commit for grep tool
    
    * ci: fix RuboCop offenses
    
    * add parse options for test cases and abilit to read args
    
    * edit style based on the formatter
    
    * ci: fix RuboCop offenses
    
    * add test cases and logic on both options for process input method
    
    * refactor based on rubocop
    
    * refactor based on rubocop again
    
    * refactor based on rubocop for the 3rd time, 3rd time charm?
    
    * refactor based on rubocop for the 4th time, cheers
    
    * chore: keep style consist
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ lib/grep.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+module GrepTool
+  def self.option_definition(options)
+    OptionParser.new do |opts|
+      opts.banner = 'Usage: grep_tool.rb [options] pattern filename'
+      opts.on('-i', '--ignore-case', 'Ignore case distinctions') do
+        options[:ignore_case] = true
+      end
+      opts.on('-v', '--invert-match', 'Invert the sense of matching') do
+        options[:invert_match] = true
+      end
+    end
+  end
+
+  def self.parse_options(args)
+    options = {
+      ignore_case: false,
+      invert_match: false
+    }
+    parser = option_definition(options)
+    parser.parse!(args)
+    [options, args]
+  end
+
+  def self.msg_for_args(args)
+    return unless args.empty?
+
+    puts 'Error: No arguments provided.'
+    puts 'Usage: ./bin/grep [options] pattern filename'
+    exit 1
+  end
+
+  def self.print_output(content, pattern, options)
+    content.each_line do |line|
+      matched = line.match?(pattern)
+
+      if options[:invert_match]
+        puts line unless matched
+      elsif matched
+        highlighted_line = line.gsub(pattern) do |match|
+          "\e[31m#{match}\e[0m"
+        end
+        puts highlighted_line
+      end
+    end
+  end
+
+  def self.process_input(content, pattern, options)
+    pattern = if options[:ignore_case]
+                Regexp.new(pattern, Regexp::IGNORECASE)
+              else
+                Regexp.new(pattern)
+              end
+
+    print_output(content, pattern, options)
+  end
+
+  def self.run_cli(argv)
+    options, args = parse_options(argv)
+
+    msg_for_args(args)
+
+    pattern = args[0]
+    filename = args[1]
+
+    if File.exist?(filename)
+      File.open(filename, 'r') do |file|
+        content = file.read
+        process_input(content, pattern, options)
+      end
+    else
+      puts "Error: File '#{filename}' does not exist."
+      exit 1
+    end
+  end
+end
---
+++ test/grep_test.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'stringio'
+require_relative '../lib/grep'
+
+describe 'Grep command - parse options' do
+  before do
+    @options = {
+      ignore_case: false,
+      invert_match: false
+    }
+  end
+
+  it 'should parse -i option' do
+    args = ['-i', 'pattern', 'test.txt']
+    options, _args = GrepTool.parse_options(args)
+
+    _(options[:ignore_case]).must_equal true
+    _(options[:invert_match]).must_equal false
+    _(args[0]).must_equal 'pattern'
+    _(args[1]).must_equal 'test.txt'
+  end
+
+  it 'should parse -v option' do
+    args = ['-v', 'pattern', 'test.txt']
+    options, _args = GrepTool.parse_options(args)
+
+    _(options[:ignore_case]).must_equal false
+    _(options[:invert_match]).must_equal true
+    _(args[0]).must_equal 'pattern'
+    _(args[1]).must_equal 'test.txt'
+  end
+end
+
+describe 'Grep command - process input - ignore case' do
+  before do
+    @content = "This is a test line.\nAnother test line.\nYet another line."
+    @pattern = 'test'
+    @options = { ignore_case: false, invert_match: false }
+    @expected_output = <<~OUTPUT
+      This is a \e[31mtest\e[0m line.
+      Another \e[31mtest\e[0m line.
+    OUTPUT
+  end
+
+  it 'should print matching lines' do
+    output = StringIO.new
+    $stdout = output
+    GrepTool.process_input(@content, @pattern, @options)
+    $stdout = STDOUT
+
+    _(output.string).must_equal @expected_output
+  end
+
+  it 'should handle case insensitivity' do
+    @pattern = 'TEST'
+    @options[:ignore_case] = true
+
+    output = StringIO.new
+    $stdout = output
+    GrepTool.process_input(@content, @pattern, @options)
+    $stdout = STDOUT
+
+    _(output.string).must_equal @expected_output
+  end
+end
+
+describe 'Grep command - process input - invert match' do
+  before do
+    @content = "This is a test line.\nAnother test line.\nYet another line."
+    @pattern = 'test'
+    @options = {
+      ignore_case: false,
+      invert_match: false
+    }
+  end
+
+  it 'should print non-matching lines when invert match is true' do
+    @options[:invert_match] = true
+    expected_output = "Yet another line.\n"
+    output = StringIO.new
+    $stdout = output
+
+    GrepTool.process_input(@content, @pattern, @options)
+
+    $stdout = STDOUT
+    _(output.string).must_equal expected_output
+  end
+end

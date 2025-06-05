9d5cd3b31f547bbd16164f4159b9eb8493cd84f8
Victoria Cheng <victoria.cheng15@gmail.com>
2025-06-05 18:33:51 -0400

    chore: move configs to its own files

---
+++ lib/configs/cat_config.rb
@@ -0,0 +1,12 @@
+CAT_OPTION_DEFS = [
+  {
+    flags: ['-n', '--number'],
+    desc: 'Number all output lines',
+    action: ->(opts, _) { opts[:number_lines] = true }
+  },
+  {
+    flags: ['-b', '--number-nonblank'],
+    desc: 'Number non-blank output lines',
+    action: ->(opts, _) { opts[:number_nonblank_lines] = true }
+  }
+].freeze
---
+++ lib/configs/grep_config.rb
@@ -0,0 +1,12 @@
+GREP_OPTION_DEFS = [
+  {
+    flags: ['-i', '--ignore-case'],
+    desc: 'Ignore case distinctions',
+    action: ->(opts, _) { opts[:ignore_case] = true }
+  },
+  {
+    flags: ['-v', '--invert-match'],
+    desc: 'Invert the sense of matching',
+    action: ->(opts, _) { opts[:invert_match] = true }
+  }
+].freeze
